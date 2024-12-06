3fb54394652e4193a4903980bce02575bbd0ae3d
megan-greatorex <68241183+megan-greatorex@users.noreply.github.com>
2024-12-06 18:09:54 -0500

    Module 10 Assignment work

--- module10_assignment/src/public/public.service.js
+++ module10_assignment/src/public/public.service.js
@@ -8,12 +8,9 @@
         var publicService = this;
 
         publicService.getFavoriteItem = function (menuItem) {
-            console.log("MENU ITEM: ", menuItem);
             let upper = $filter('uppercase')(menuItem);
             let shortName = upper.toString().match("[a-zA-Z]+")[0];
-            console.log("SHORT NAME: ", shortName);
             let item = upper.toString().split(shortName)[1] == "" ? 0 : upper.toString().split(shortName)[1];
-            console.log("ITEM: ", item);
             return $http.get(BasePath + '/menu_items/' + shortName + '/menu_items/' + item + '.json')
                 .then(function (response) {
                     return response;
